65830089ee3b5146e677623e00937d52331d3695
L33s <jianhui0614@gmail.com>
2017-03-14 11:26:41 +0800

    Add files via upload

--- Crash_B_Design/Scripts/initial_.js
+++ Crash_B_Design/Scripts/initial_.js
@@ -3,9 +3,10 @@
     $(".button-collapse").sideNav({
         menuWidth:"25%"
     });
+    $('.parallax').parallax();
+  
     moving_boxes();
     mb_id = setInterval(moving_boxes, 3000);
-    mouse_moving_boxes(mb_id);
 
 })
 
@@ -27,14 +28,14 @@ function random_moving_boxes() {
     });
 
 }
-var t_point = [0.6,0.3,0.0,0.3];
-var l_point = [0.45, 0.80, 0.45, 0.1];
+var t_point = [0.8,0.0,0.0,0.8];
+var l_point = [0.1, 0.1, 0.8, 0.8];
 var active_ = true;
 function moving_boxes() {
     if (active_) {
         $(".frame-o").each(function (index) {
             var win_wid = $(".main_").width();
-            var win_hei = $(".main_").height();
+            var win_hei = 550;
             var box_wid = win_wid * 0.1;
             var box_rnd = random(50, box_wid);
             $(this).css({
@@ -59,35 +60,7 @@ function moving_boxes() {
     }
     
 }
-function mouse_moving_boxes(mb_id) {
-    
-    $(".main_").mouseover(function (e) {
-        active_ = false;
-        var tgr_off = $(".main_").offset();
-        var x_ = Math.round(e.clientX - tgr_off.left);
-        var y_ = Math.round(e.clientY);
-        $(".frame-o").each(function (index) {
-            var win_wid = $(".main_").width();
-            var win_hei = $(".main_").height();
-            var box_wid = win_wid * 0.1;
-            var box_rnd = random(50, box_wid);
-            $(this).css({
-                "left": x_ + "px",
-                "top": y_ + "px",
-                "width": box_wid + "px",
-                "height": box_wid + "px",
-                //"border-width": random(5, 15) + "px",
 
-                //"border-radius": random(5, 45) + "%"
-            })
-
-        });
-        $(".main_").mouseleave(function () { active_ = true });
-        $("#disply_1").text(x_ + "," + y_);
-        $("#disply_2").text(tgr_off.left + "," + tgr_off.top);
-
-    });
-}
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
